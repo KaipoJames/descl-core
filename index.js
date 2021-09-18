dd9d852462e78c9fd8553c2afe2af0a8f15711a6
Kaipo <kaipojames12@gmail.com>
2021-09-18 09:23:13 -0500

    Set up Express Server with connection to MongoDB

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-import Parser from "./src/parser.js";
-Parser.init();
+// import Parser from "./src/parser.js";
+// Parser.init();
+
+// DEPENDENCIES
+import express from 'express';
+import path from "path";
+import dotenv from 'dotenv';
+dotenv.config();
+import pkg from 'mongodb';
+const { MongoClient } = pkg;
+
+const app = express();
+const __dirname = path.resolve();
+import router from "./backend/routes/router.js" ;
+
+// Middleware
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+app.use(express.static("public/"));
+app.use(express.urlencoded({ extended: true })); // Enable our form data to be accessed by the 'req' variable in our routes
+app.use(express.json());
+app.use("/", router);
+
+
+app.listen(process.env.PORT || 5000, (err, res) => {
+    if (err) console.log(err);
+    console.log("Server is listening on port 5000 at: http://localhost:5000");
+
+    const mongoClient = new MongoClient(process.env.MONGO_CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoClient.connect((err, client) => {
+        if (err) { console.error(err); }
+        console.log("Connected To MongoDB");
+    });
+
+});
